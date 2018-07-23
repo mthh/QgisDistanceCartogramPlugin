93a19aa7d340576af2c9f895c262bbcd79a3d8e0
mthh <matthieu.viry@cnrs.fr>
2018-07-23 15:05:13 +0200

    Translation + Dialog layout

--- i18n/DistCartogram_fr.ts
+++ i18n/DistCartogram_fr.ts
@@ -3,170 +3,255 @@
 <context>
     <name>DistCartogram</name>
     <message>
-        <location filename="../dist_cartogram.py" line="222"/>
+        <location filename="../dist_cartogram.py" line="261"/>
         <source>&amp;DistCartogram</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="213"/>
+        <location filename="../dist_cartogram.py" line="252"/>
         <source>Create distance cartogram</source>
         <translation>&gt;Création de cartogramme de distance</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="353"/>
+        <location filename="../dist_cartogram.py" line="592"/>
         <source>Error</source>
         <translation>Erreur</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="353"/>
+        <location filename="../dist_cartogram.py" line="480"/>
         <source>An error occurred during distance cartogram creation. Please see the &#xe2;&#x80;&#x9c;Plugins&#xe2;&#x80;&#x9d; section of the message log for details.</source>
-        <translation type="unfinished"></translation>
+        <translation>Une erreur est survenue durant la création du cartogramme de distance. Des détails sont disponibles dans la section "Plugins" du journal des messages.</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="398"/>
+        <location filename="../dist_cartogram.py" line="528"/>
         <source>DistCartogram computation cancelled by user</source>
         <translation>Calcul annulé par l&apos;utilisateur</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="432"/>
+        <location filename="../dist_cartogram.py" line="545"/>
         <source>Starting...</source>
         <translation>Calcul en cours...</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="437"/>
+        <location filename="../dist_cartogram.py" line="549"/>
         <source>Cancel</source>
         <translation>Annulation</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="447"/>
+        <location filename="../dist_cartogram.py" line="559"/>
         <source>Starting</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../dist_cartogram.py" line="238"/>
         <source>No match between ids</source>
-        <translation>Pas de correspondance entre les ID de la couche et ceux de la matrice</translation>
+        <translation type="obsolete">Pas de correspondance entre les ID de la couche et ceux de la matrice</translation>
     </message>
     <message>
         <location filename="../dist_cartogram.py" line="254"/>
         <source>File not found</source>
-        <translation>Fichier non trouvé</translation>
+        <translation type="obsolete">Fichier non trouvé</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram.py" line="463"/>
+        <location filename="../dist_cartogram.py" line="583"/>
         <source>1- Creation of image points layer</source>
-        <translation>1 - Création de la couche de points "image"</translation>
+        <translation>1 - Création de la couche de points &quot;image&quot;</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="290"/>
+        <source>No match between point layer ID and matrix ID</source>
+        <translation type="unfinished">Pas des correspondance entre les identifiants de la couche de points et ceux de la matrice</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="294"/>
+        <source>Success</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="294"/>
+        <source>Matches found between point layer ID and matrix ID</source>
+        <translation type="unfinished">correspondances trouvées entre les identifiants de la couche de points et ceux de la matrice</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="311"/>
+        <source>Identifiant values have to be uniques</source>
+        <translation type="unfinished">Les valeurs d'identifiants doivent être uniques.</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="316"/>
+        <source>Not enough matching features between source and image layer</source>
+        <translation type="unfinished">Pas assez de correspondances trouvées entre la couche source et la couche image</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="333"/>
+        <source>File {} not found</source>
+        <translation>Ficher {} non trouvé</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="355"/>
+        <source>Error while reading the matrix - All values (excepting columns/lines id) must be numbers</source>
+        <translation>Erreur lors de la lecture de la matrice. Toutes les valeurs (sauf les identifiants de ligne/colonne) doivent être des nombres</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="362"/>
+        <source>An unexpected error has occurred while reading the CSV matrix. Please see the &#xe2;&#x80;&#x9c;Plugins&#xe2;&#x80;&#x9d; section of the message log for details.</source>
+        <translation>Une erreur innatendue est survenue pendant la lecture du fichier CSV. Des détails sont disponibles dans la section "Plugins" du journal des messages.</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="376"/>
+        <source>Lines and columns index have to be the same</source>
+        <translation>Les identifiants présents sur les lignes et les colonnes doivent être les mêmes.</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="592"/>
+        <source>DistCartogram: The &quot;image&quot; point layer is empty.This is probably due to a problem of non-correspondence between the identifiers of the features the point layer and the identifiers in the provided matrix.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram.py" line="637"/>
+        <source>Number of features differ between source and image layers - Only feature with matching ids will be taken into account</source>
+        <translation type="unfinished">Le nombre d'entités est différent entre la couche "source" et la couche "image" - Seulement les entités ayant des identifiants qui correspondent seront prise en compte.</translation>
     </message>
 </context>
 <context>
     <name>DistCartogramDialogBase</name>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="136"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="272"/>
         <source>DistCartogram</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="137"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="273"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Distance cartogram creation &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-size:12pt; font-weight:600;&quot;&gt;Création de cartogramme de distance &lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="144"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="293"/>
         <source>Grid precision</source>
         <translation>Précision de la grille</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="150"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="276"/>
         <source>Point layer</source>
         <translation>Couche de points (source)</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="149"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="282"/>
         <source>Displacement factor</source>
-        <translation>Facteur de déplacment</translation>
+        <translation>Facteur de déplacement</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="139"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="279"/>
         <source>Time matrix</source>
         <translation>Matrice de temps</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="140"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="277"/>
         <source>Point layer id field</source>
         <translation>Champ d&apos;identifiant (couche de points)</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="152"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="296"/>
         <source>Background layer</source>
         <translation>Couche à déformer</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="142"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="281"/>
         <source>Reference feature</source>
         <translation>Entité de référence</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="145"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="290"/>
         <source>Transformed layer</source>
         <translation>Couche(s) transformée(s)</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="146"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="286"/>
         <source>Image point layer</source>
         <translation>Points &quot;image&quot;</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="147"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="291"/>
         <source>Source grid</source>
         <translation>Grille d&apos;origine</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="148"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="292"/>
         <source>Transformed grid</source>
         <translation>Grille déformée</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="143"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="294"/>
         <source>Output</source>
         <translation>Sortie(s)</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="138"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="297"/>
         <source>Path to a .csv document containing a time matrix between the points from the layer previously selected.</source>
         <translation>Chemin vers un fichier au format .csv et contenant une matrice de distance/temps de parcours entre les points de la couche précédemment sélectionnée.</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="141"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="280"/>
         <source>The reference feature (it&apos;s location will stay unchanged)</source>
-        <translation>L'entité de référence (sa position restera inchangée).</translation>
+        <translation>L&apos;entité de référence (sa position restera inchangée).</translation>
     </message>
     <message>
-        <location filename="../dist_cartogram_dialog_baseUi.py" line="151"/>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="295"/>
         <source>The layer to be deformed</source>
         <translation>La couche à déformer</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="289"/>
+        <source>From source points and time matrix</source>
+        <translation>À partir d'une couche de points et d'une matrice</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="298"/>
+        <source>Image points layer</source>
+        <translation>Couche de points "image"</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="299"/>
+        <source>Source points layer</source>
+        <translation>Couche de points "source"</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="300"/>
+        <source>Source points layer id field</source>
+        <translation>Champ d'identifiants de la couche "source"</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="301"/>
+        <source>Image points layer id field</source>
+        <translation>Champ d'identifiants de la couche "image"</translation>
+    </message>
+    <message>
+        <location filename="../dist_cartogram_dialog_baseUi.py" line="302"/>
+        <source>From source points and image points</source>
+        <translation>À partir de points "source" et de points "image"</translation>
     </message>
 </context>
 <context>
     <name>DistCartogramWorker</name>
     <message>
-        <location filename="../worker.py" line="141"/>
-        <source>2 - Creation of interpolation grid...</source>
-        <translation>2 - Création de la grille d&apos;interpolation</translation>
-    </message>
-    <message>
-        <location filename="../worker.py" line="143"/>
-        <source>3 - Interpolation process...</source>
-        <translation>3 - Interpolation en cours</translation>
-    </message>
-    <message>
-        <location filename="../worker.py" line="145"/>
-        <source>4 - Transforming layers...</source>
-        <translation>4 - Transformation des couches...</translation>
-    </message>
-    <message>
         <location filename="../worker.py" line="148"/>
-        <source>5 - Preparing results for displaying...</source>
-        <translation>5 - Préparation des résultats  pour l&apos;affichage...</translation>
+        <source>Transforming layers...</source>
+        <translation>Transformations des couches...</translation>
+    </message>
+    <message>
+        <location filename="../worker.py" line="142"/>
+        <source>Creation of interpolation grid...</source>
+        <translation>Création de la grille d'interpolation</translation>
+    </message>
+    <message>
+        <location filename="../worker.py" line="144"/>
+        <source>Interpolation process...</source>
+        <translation>Interpolation en cours</translation>
+    </message>
+    <message>
+        <location filename="../worker.py" line="151"/>
+        <source>Preparing results for displaying...</source>
+        <translation>Préparation des résultats pour l'affichage</translation>
     </message>
 </context>
 </TS>
